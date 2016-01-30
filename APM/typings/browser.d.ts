619ddadba67a231db34f5d9c9d001b2a9c4dc07b
Eliseo Calderon <Eliseo Calderon>
2016-01-30 15:51:35 -0600

    Course Complete

---
+++ APM/typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser\ambient\angular-mocks\angular-mocks.d.ts" />
+/// <reference path="browser\ambient\angular-resource\angular-resource.d.ts" />
+/// <reference path="browser\ambient\angular-route\angular-route.d.ts" />
+/// <reference path="browser\ambient\angular\angular.d.ts" />
+/// <reference path="browser\ambient\jquery\jquery.d.ts" />
---
+++ APM/typings/main.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="main\ambient\angular-mocks\angular-mocks.d.ts" />
+/// <reference path="main\ambient\angular-resource\angular-resource.d.ts" />
+/// <reference path="main\ambient\angular-route\angular-route.d.ts" />
+/// <reference path="main\ambient\angular\angular.d.ts" />
+/// <reference path="main\ambient\jquery\jquery.d.ts" />
